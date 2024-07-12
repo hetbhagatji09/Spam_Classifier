66e4b96e6b62bffe84648f266755877d3209a846
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-07-12 23:11:03 +0530

    modification in logger

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -8,20 +8,22 @@ def error_message_detail(error,error_detail:sys):
     err_mess = str(error)
     error_message = "Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(file_name, line_no, err_mess)
     return error_message
-    
-    
-    
-    
 
+    
+    
+    
+    
 class customException(Exception):
     def __init__(self,error_message, error_details : sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_details=error_details)
+        self.error_message=error_message_detail(error_message,error_details)
         
     def __str__(self):
         return self.error_message
-        
-# try:
-#     x=1/0,
-# except Exception as e:
-#     raise customException(e,sys)
+ 
+if __name__ == "__main__":        
+    try:
+        x=1/0,
+    except Exception as e:
+        logging.info('Divide by zero')
+        raise customException(e,sys)
